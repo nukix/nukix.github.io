0cc15bf7637176e78de25836f7d377e9d85db267
nukix <aaa539687357@vip.qq.com>
2024-04-27 20:12:38 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -15,8 +15,8 @@ app.use(pinia);
 app.mount("#app");
 
 // PWA
-navigator.serviceWorker.addEventListener("controllerchange", () => {
-  // 弹出更新提醒
-  console.log("站点已更新，刷新后生效");
-  ElMessage("站点已更新，刷新后生效");
-});
+// navigator.serviceWorker.addEventListener("controllerchange", () => {
+//   // 弹出更新提醒
+//   console.log("站点已更新，刷新后生效");
+//   ElMessage("站点已更新，刷新后生效");
+// });
